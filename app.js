45bfab1bc6261cf277c26301a6effaaa63404038
Muhammad Waqas Rizvi <waqashafiz90@gmail.com>
2021-03-23 16:00:00 +0500

    Try & Catch

---
+++ app.js
@@ -0,0 +1,116 @@
+// var a = "Waqas"
+// var b = "Altaf"
+// alert(a + " " + b)
+
+// var a = "Waqas"
+// var a = "Hassan"
+// var b = "Altaf"
+// alert(a + " " + b)
+
+// let a = "Waqas"
+// a = "Hassan"
+// let b = "Altaf"
+// alert(a + " " + b)
+
+// let a = "Waqas"
+// let a = "Hassan"
+// let b = "Altaf"
+// console.log(a)
+// alert(a + " " + b)
+
+// const a = "Waqas"
+// a = "Hassan"
+// const b = "Altaf"
+// alert(a + " " + b)
+// console.log(a)
+
+
+//Pass by Value
+
+// let c = "Waqas"
+// let d = c 
+// d = "Hamza"
+// console.log(c)
+// console.log(d)
+
+
+//Pass by Reference
+
+// let a = [1,2,3,4]
+// let b = a 
+// b.push(5)
+// console.log(a)
+// console.log(b)
+
+// let a = [1,2,3,4]
+// let b = [].concat(a) 
+// b.push(5)
+// console.log(a)
+// console.log(b)
+
+
+//Rest Operator and Destructuring
+
+// let a, b;
+// [a, b] = [4, 5]
+// console.log(a, b)
+
+// let a, b;
+// [a, ...b] = [1,2,3,4,5,6]
+// console.log(a,b)
+
+// let food = ["Zinger", "Broast", "Roll"]
+// let a,b,c;
+// // [a, b, c] = food;
+// [a, ...b] = food;
+// // console.log(a ,b , c)
+// console.log(a ,b)
+
+
+//Spread Operator
+
+// let array1 = ['h', 'e', 'y'];
+// let array2 = [...array1];
+// array2.push("w")
+// console.log(array1);
+// console.log(array2);
+
+
+//Template Literals 
+
+// let a = "waqas Rizwi"
+// // console.log("My name is " + a)
+// console.log(`My name is ${a}`)
+
+
+//Object Literal Shorthand
+
+// const a = 'foo';
+// const b = 42;
+// const c = {
+//     a: "w",
+//     b: "a"
+// };
+// const object1 = {
+//     a: a,
+//     b: b,
+//     c: c
+// };
+// console.log(object1);
+
+// const object2 = {
+//     a,
+//     b,
+//     c
+// };
+// console.log(object2);
+
+
+//Try & Catch
+
+// try{
+//     aler("Waqas")
+// } catch(error){
+//     console.log(`This is ${error}`)
+// }
+
